580a7a556560c12dc8dcc95926b2b122c9519585
ken <yash20004x17@gmail.com>
2025-08-23 05:01:16 +0000

    Completed Insertion Sort and Sorting Technique part 1 from Striver in JAVA

--- Main.java
+++ Main.java
@@ -642,9 +642,24 @@ class Main {
         }
     }
 
+    // Third => Insertion SORT
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1,2,3,4,5};
-        bubbleSort(arr);
+        int[] arr = { 5, 4, 3, 2, 1, 1, 2, 3, 4, 5 };
+        insertionSort(arr);
 
     }
 }
