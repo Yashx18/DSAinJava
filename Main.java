53a8ea95c5755da418de65ac15eb460c94cd9bd6
ken <yash20004x17@gmail.com>
2025-09-01 06:58:19 +0000

    Completed 2 problems of Arrays video 2 from Striver

--- Main.java
+++ Main.java
@@ -806,11 +806,24 @@ class Main {
         }
     }
 
+    // Fifth => Left rotate the Array by one place
+    static void leftRotate(int[] arr, int d) {
+        for (int i = 0; i < d; i++) {
+            int temp = arr[i];
+            for (int j = d; j < arr.length; j++) {
+                arr[j - d] = arr[j];
+            }
+            arr[arr.length - 1] = temp;
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 2, 2, 3, 3, 3, 4, 5, 5, 6 ,7};
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int d = 2;
         // int low = 0;
         // int high = arr.length - 1;
-        removeDuplicates(arr);
+        leftRotate(arr, d);
         // System.out.println(Arrays.toString(arr));
     }
 }
